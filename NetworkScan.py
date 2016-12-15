199e6e549d2993f93dd5e2277de680b3c9b6b4df
Lagicrus <Lagicrus@users.noreply.github.com>
2016-12-15 16:46:43 +0000

    Update NetworkScan.py

--- NetworkScan.py
+++ NetworkScan.py
@@ -4,6 +4,7 @@ from ctypes import windll, create_string_buffer
 class all():
     def LANScan():
         global net_addr, all_hosts, started
+        #Get the IP Net to scan
         net_addr = input("Enter a network address (ex.192.168.1.0): ")
         print("")
         net_addr = net_addr + "/24"
@@ -24,13 +25,14 @@ class all():
         info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
         info.wShowWindow = subprocess.SW_HIDE
         # For each IP address in the subnet,
-        # run the ping command with subprocess.popen interface
         iw = 0
         iq = 0
         for i in range(len(all_hosts)):
             output = subprocess.Popen(['ping', '-n', '1', '-w', '500', str(all_hosts[i])], stdout=subprocess.PIPE, startupinfo=info).communicate()[0]
+            #If the host cannot be reached eg doesnt respond
             if "Destination host unreachable" in output.decode('utf-8'):
                 iw += 1
+            #If the host takes to long to respond
             elif "Request timed out" in output.decode('utf-8'):
                 iq += 1
             else:
@@ -51,11 +53,13 @@ class all():
         info = subprocess.STARTUPINFO()
         info.dwFlags |= subprocess.STARTF_USESHOWWINDOW
         info.wShowWindow = subprocess.SW_HIDE
+        #Snips of the front of the URL so that it can be pinged
         if "http://" in IP:
             IP = IP[7:]
         elif "https://" in IP:
             IP = IP[8:]
         output = subprocess.Popen(["ping","-n","1",str(IP)],stdout=subprocess.PIPE,startupinfo=info).communicate()[0]
+        #If any errors occur with Ping then deal with it
         if "Ping request could not find host" in output.decode("utf-8"):
             print(formatt.format("\nPlease enter a valid IP address\n"))
             started = "TrueP"
@@ -76,6 +80,7 @@ class all():
 
     def reply():
         global started, finished
+        #This is the stupid way in which it grabs the details to reply with later when it prints out the stats
         for line in output.decode("utf-8").splitlines():
             if "Approximate" in line:
                 mini = line
@@ -107,14 +112,17 @@ class all():
                 loss = loss[:1]
         print("")
         print(formatt.format("It took %s hops to get to its destination"%(64 - TTL)))
+        #print(formatt.format("It went through %s routes and switches to get there"%(64 - TTL)))
         print(formatt.format("Average time to destination is %s"%(averagems)))
         print(formatt.format("Sent %s packets to get their with a %s%% loss"%(packets,loss)),"\n")
         finished = True
         started = False
     def port():
+        #The popular ports that you might ping
         portlist = {1:"TCP",5:"RJE",7:"ECHO",18:"MSP",20:"FTP Data",21:"FTP Control",22:"SSH",23:"Telnet",25:"SMTP",45:"Internet Message Protocol",47:"NI FTP",53:"DNS",57:"MTP",66:"Oracle SQL*NET",80:"HTTP",89:"SU/MIT Telnet Gateway",92:"Network Printing Protocol",93:"Device Control Protocol",107:"Remote Telnet Service",109:"POP2",110:"POP3",115:"SFTP",117:"UUCP",118:"SQL",143:"IMAP",156:"SQL Service/Server",161:"SNMP",194:"IRC",197:"DLS",443:"HTTPS",546:"DHCP Client",547:"DHCP Server",8080:"HTTP Alt",8008:"HTTP Alt"}
         global IP, Port, started
         PortCheck = False
+        #Grab the IP to port test
         IP = input(formati.format("Enter IP: ")).lower()
         while PortCheck != True:
             Port = input(formati.format("Enter Port: "))
@@ -131,6 +139,7 @@ class all():
         print("Starting Scan")
         print("")
         s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        #If the response is A then test ALL PORTS
         if Port == "A":
             for portA in range(1,65000):
                 result = s.connect_ex((IP,portA))
@@ -144,6 +153,7 @@ class all():
                         print("%s:%s is open"%(IP,portA))
                     else:
                         print("%s:%s is closed"%(IP,portA))
+        #If it knows what the port is
         elif Port in portlist:
             result = s.connect_ex((IP,Port))
             if result == 0:
@@ -163,7 +173,9 @@ class all():
 
 def startup():
     global idlecheck, centre, formatt, formati
+    #If it is running in console which is detected by the idlelib.run not being in the running modules
     if "idlelib.run" not in sys.modules:
+        #figure out the width of the screen
         h = windll.kernel32.GetStdHandle(-12)
         csbi = create_string_buffer(22)
         resolution = windll.kernel32.GetConsoleScreenBufferInfo(h,csbi)
@@ -175,6 +187,7 @@ def startup():
         formatt = "{:^%s}"%(centre-1)
         formati = "{:>%s}"%(int(centre/2))
     else:
+        #else, default settings
         idlecheck = False
         formatt = "{:^70}"
         formati = "{:>45}"
@@ -184,6 +197,7 @@ def choice():
     print("")
     choc = False
     while choc != True:
+        #Gets the user to enter a option that they want to use
         cho = input(formati.format("P or L or K: ")).lower()
         if cho == "ping" or cho == "p":
             all.ping()
@@ -195,8 +209,7 @@ def choice():
             all.port()
             choc = True
         else:
-            print("Not a valid option")
-            print("")
+            print("Not a valid option\n")
 
 def end():
     global finished
@@ -204,6 +217,7 @@ def end():
         finished = False
 
 def argument(argv):
+    #If it was run with arguments
     global IP
     global Port
     global all_hosts
@@ -213,9 +227,11 @@ def argument(argv):
     except getopt.GetoptError:
         print("Example.py -p <IP/URL> -k <Port> -l <LAN eg 192.168.0.0>")
     for opt, arg in opts:
+        #If they asked for help
         if opt in ("-h", "--help", "-?", "/?", "\?"):
             print("Example.py -p <IP/URL> -k <Port> -l <LAN eg 192.168.0.0>")
             sys.exit()
+        #If they are using the lan scan setting
         elif opt in ("-l", "--lan"):
             try:
                 net_addr = arg
@@ -227,6 +243,7 @@ def argument(argv):
                 print("You did not enter a valid IP")
                 print("")
                 started = True
+        #If they want to port test
         elif opt in ("-k", "--knock"):
             try:
                 Port = int(arg)
@@ -250,9 +267,11 @@ global started
 finished = False
 started = False
 
+#If it detects you are running in terminal
 if "idlelib.run" not in sys.modules:
         print("CONSOLE MODE")
 
+#If it detets you are trying to run with arguments
 if len(sys.argv) > 1:
     startup()
     argument(sys.argv[1:])
